c5c294de85b732fcd128d1f87905bbd047bf4cdf
rubythecutie <228643222+rubythecutie@users.noreply.github.com>
2026-07-02 18:36:30 +1200

    Add basic anti-censorship for API

--- vpngate/api.py
+++ vpngate/api.py
@@ -2,6 +2,9 @@ import base64
 import requests
 import json
 from pathlib import Path
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def get_port(config):
     config = base64.b64decode(config).decode('utf-8').split("\n")
@@ -32,19 +35,58 @@ def servers():
             x.write(json.dumps(content))
     return content
 
+def try_default_api():
+    servers = {}
+    try:
+        data = requests.get("http://www.vpngate.net/api/iphone/", verify=False).text.split("\n")[2:-2]
+
+        for i in data:
+            temp = i.split(",")
+            servers[temp[0]] = {
+                "ip": temp[1].strip(),
+                "country": temp[6].strip(),
+                "openvpn": temp[-1].strip(),
+                "port": get_port(temp[-1]).strip(),
+                "protocol": get_protocol(temp[-1]).strip()
+            }
+        
+        print("Success")
+    except:
+        print("Failed")
+    
+    return servers
+
+def try_github_mirrors():
+    server_data = {}
+    mirrors = [
+        "https://raw.githubusercontent.com/fdciabdul/Vpngate-Scraper-API/refs/heads/main/json/data.json",
+        "https://raw.githubusercontent.com/bagussatoto/Vpngate-Scraper-API/refs/heads/main/json/data.json"
+    ]
+
+    for i in mirrors:
+        print(f"Trying {i}...", end=" ")
+        try:
+            data = requests.get(i, verify=False)
+            data = json.loads(data.text)[0]["servers"]
+            for j in data:
+                server_data[j["hostname"]] = {
+                    "ip": j["ip"].strip(),
+                    "country": j["countryshort"].strip(),
+                    "openvpn": j["openvpn_configdata_base64"].strip(),
+                    "port": get_port(j["openvpn_configdata_base64"]).strip(),
+                    "protocol": get_protocol(j["openvpn_configdata_base64"]).strip()
+                }
+
+            print("Success")
+            return server_data
+        except:
+            print("Failed")
 
 def fetch_servers():
-    servers = {}
-    data = requests.get("http://www.vpngate.net/api/iphone/").text.split("\n")[2:-2]
-
-    for i in data:
-        temp = i.split(",")
-        servers[temp[0]] = {
-            "ip": temp[1].strip(),
-            "country": temp[6].strip(),
-            "openvpn": temp[-1].strip(),
-            "port": get_port(temp[-1]).strip(),
-            "protocol": get_protocol(temp[-1]).strip()
-        }
-
-    return servers
+    print("Trying default API...", end=" ")
+    server_data = try_default_api()
+    if server_data == {}:
+        server_data = try_github_mirrors()
+    else:
+        return {}
+    return server_data
